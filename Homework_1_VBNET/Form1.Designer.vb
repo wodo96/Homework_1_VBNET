facca6d9ad561f4811907f66be0497a4d73e748f
wodo96 <renzo.cuomo1996@gmail.com>
2022-10-05 16:49:39 +0200

    FinalCommit

--- Homework_1_VBNET/Form1.Designer.vb
+++ Homework_1_VBNET/Form1.Designer.vb
@@ -22,10 +22,97 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(23, 50)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(110, 19)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Enable the form"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(313, 28)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(122, 46)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Write the text that you want to save."
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(172, 80)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(404, 174)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(226, 269)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(449, 269)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Empty"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Location = New System.Drawing.Point(296, 310)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(170, 80)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
 End Class
--- Homework_1_VBNET/Form1.vb
+++ Homework_1_VBNET/Form1.vb
@@ -1,3 +1,84 @@
-﻿Public Class Form1
+﻿Imports System.Runtime.CompilerServices
 
+Public Class Form1
+
+
+    Dim isBeenSaved As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.RichTextBox1.Visible = False
+        Me.RichTextBox1.Enabled = False
+        Me.Button1.Visible = False
+        Me.Button1.AutoSize = True
+        Me.Button2.Visible = False
+
+    End Sub
+
+
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+        If (CheckBox1.Checked) Then
+            Me.RichTextBox1.Visible = True
+            Me.RichTextBox1.Enabled = True
+        Else
+            Me.RichTextBox1.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+        If (RichTextBox1.Text = "" & Not (Me.isBeenSaved)) Then
+            Me.Button1.Visible = False
+            Me.Button2.Visible = False
+        Else
+            Me.Button1.Visible = True
+            Me.Button2.Visible = True
+        End If
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If (Not (Me.RichTextBox1.Text = "")) Then
+            Me.isBeenSaved = True
+            Me.TextBox2.Visible = True
+            Me.TextBox2.Text = Me.RichTextBox1.Text
+            Me.Button1.BackColor = System.Drawing.Color.Green
+            Me.Button1.Text = "Saved!"
+
+        Else
+
+            Me.Button1.Text = "You should write something at least!"
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_MouseHover(sender As Object, e As EventArgs) Handles Button1.MouseHover
+        Me.Button1.Text = "Click me!"
+    End Sub
+
+    Private Sub Button1_MouseLeave(sender As Object, e As EventArgs) Handles Button1.MouseLeave
+
+        If (Not (Me.Button1.BackColor.Equals(System.Drawing.Color.Green))) Then
+
+            Me.Button1.Text = "Save"
+
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Me.RichTextBox1.Text = ""
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Text = "Save"
+        Me.TextBox2.Visible = False
+
+    End Sub
 End Class
